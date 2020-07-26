127dfe474e29688fa99a68e9d4cec04613a8023f
Gryphy <hippogryphy@hotmail.com>
2020-07-26 22:35:50 +0200

    Updates

---
+++ precache-manifest.820d862196f137c37d1429ac5eb948c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2af1cb268474ed0783dd6e8db45a7fe",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f303ee6ccd70e8149449",
+    "url": "/monsters-rolodex/static/css/main.e79fed02.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f303ee6ccd70e8149449",
+    "url": "/monsters-rolodex/static/js/main.0d6684e1.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
